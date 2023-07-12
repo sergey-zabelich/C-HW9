96e74aa35a9c802c6c979a5590f48ecefe57ea8d
Sergey <zabelich19@gmail.com>
2023-07-12 15:30:51 +0300

    Задайте значение M и N. Напишите программу,которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии

---
+++ Задача_01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void che(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    che(m + 1, n);
+
+}
+che(m, n);
